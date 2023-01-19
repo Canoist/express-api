57de96a8eec1112b1a0176af6280f075f39ee253
Leonid Lichkun <lionproklon@gmail.com>
2023-01-19 14:48:27 +0300

    Added Logger, prepare for pretty print logs

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,20 @@
+import { Logger, BaseLogger } from "tslog";
+
+export class LoggerService {
+    public logger: Logger<unknown>;
+    constructor() {
+        this.logger = new Logger();
+    }
+
+    log(...args: unknown[]) {
+        this.logger.info(...args);
+    }
+
+    warn(...args: unknown[]) {
+        this.logger.warn(...args);
+    }
+
+    error(...args: unknown[]) {
+        this.logger.error(...args);
+    }
+}
