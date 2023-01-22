64b8c27e141e72b1c0bafcbcf90f79a3fbba4252
Leonid Lichkun <lionproklon@gmail.com>
2023-01-22 16:22:54 +0300

    Added instruction for using a clinic.js doctor, changed gitignore

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -9,14 +9,6 @@ import "reflect-metadata";
 import { HTTPError } from "../errors/http-error.class";
 import { IUserController } from "./user.interface";
 
-class User {
-    name: string;
-    constructor(name: string) {
-        this.name = name;
-    }
-}
-const users = [];
-
 @injectable()
 export class UserController extends BaseControler implements IUserController {
     path: string;
@@ -29,9 +21,9 @@ export class UserController extends BaseControler implements IUserController {
     }
 
     login(req: Request, res: Response, next: NextFunction): void {
-        users.push(new User("new User" + Math.random()));
         next(new HTTPError(401, "Ошибка авторизации", "login"));
     }
+
     register(req: Request, res: Response, next: NextFunction): void {
         this.ok(res, "register");
     }
