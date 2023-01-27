3e13a900c53b34babd4d3584236b7ebd56b71571
Leonid Lichkun <lionproklon@gmail.com>
2023-01-27 19:05:12 +0300

    revert last commit, add coverage folder to .gitignore [FROM LAST COMMIT] remove 'folder' param from e2e.config, because we have testRegex, added param coverage to command for test:e2e

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
     preset: "ts-jest",
     verbose: true,
-    rootDir: "./tests",
     testRegex: ".e2e-spec.ts$",
 };
 
