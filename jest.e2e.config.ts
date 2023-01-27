921ff50d869fab82d481c7c02b3c02e682e45307
Leonid Lichkun <lionproklon@gmail.com>
2023-01-27 19:02:40 +0300

    Revert "remove 'folder' param from e2e.config, because we have testRegex, added param coverage to command for test:e2e"
    This reverts commit 81da4997ea254b89b67062257c4bbd22318615af.

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
     preset: "ts-jest",
     verbose: true,
+    rootDir: "./tests",
     testRegex: ".e2e-spec.ts$",
 };
 
