88e07a45289e9901828c766b9cc04f433f36b3a3
Leonid Lichkun <lionproklon@gmail.com>
2023-01-27 18:53:10 +0300

    Added last tests for all path of users path

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -14,7 +14,55 @@ describe("Users e2e", () => {
         const res = await request(application.app)
             .post("/users/register")
             .send({ name: "Bolfd", email: "b@4.ru", password: "somePassword" });
+
         expect(res.statusCode).toBe(422);
+    });
+
+    it("Login-success", async () => {
+        const res = await request(application.app)
+            .post("/users/login")
+            .send({ email: "b@4.ru", password: "somePassword" });
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.jwt).not.toBeUndefined();
+    });
+
+    it("Login-error", async () => {
+        const res = await request(application.app)
+            .post("/users/login")
+            .send({ email: "b@4.ru", password: "someWrongPassword" });
+
+        expect(res.statusCode).toBe(401);
+    });
+
+    it("info-success", async () => {
+        const { body } = await request(application.app)
+            .post("/users/login")
+            .send({ email: "b@4.ru", password: "somePassword" });
+
+        const res = await request(application.app)
+            .get("/users/info")
+            .set("Authorization", "Bearer " + body.jwt)
+            .send({ email: "b@4.ru", password: "somePassword" });
+
+        expect(body.jwt).not.toBeUndefined();
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.email).not.toBeUndefined();
+    });
+
+    it("info-error", async () => {
+        const { body } = await request(application.app)
+            .get("/users/login")
+            .set({})
+            .send({ email: "b@4.ru", password: "somePassword" });
+
+        const res = await request(application.app)
+            .get("/users/info")
+            .set("Authorization", "Bearer " + body.jwt + "error")
+            .send({ email: "b@4.ru", password: "somePassword" });
+
+        expect(res.statusCode).toBe(401);
     });
 });
 
